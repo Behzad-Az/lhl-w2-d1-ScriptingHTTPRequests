46e773506b2c294dcb021a1d574e032eb26e6b88
Behzad Azampour <behy1368@gmail.com>
2016-11-07 19:48:05 +0000

    commiting working version of step 3

---
+++ step3PrintingWithParameters.js
@@ -0,0 +1,29 @@
+function getAndPrintHTML (options) {
+
+
+  /* Add your code here */
+  var http = require('https');
+  var link = options.host + options.path;
+  var output = '';
+
+  http.get(link, (httpResponse) => {
+    httpResponse.setEncoding('utf8');
+
+    httpResponse.on('data', function (chunk) {
+      output += chunk;
+    });
+
+    httpResponse.on('end', () => {
+      console.log(output);
+    });
+
+  });
+
+}
+
+var requestOptions = {
+  host: 'https://sytantris.github.io',
+  path: '/http-examples/step2.html'
+};
+
+getAndPrintHTML(requestOptions);
